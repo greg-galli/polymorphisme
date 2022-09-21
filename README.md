3c45fcd556b35e1dfaf7fd16e8c3890ad4eebbde
Gregory Galli <greg.galli@gmail.com>
2022-09-21 09:40:29 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# polymorphisme
+# Ennoncé
+Pour valider cet exercice, vous devez écrire toutes les classes necessaire à la bonne exécution de ce projet.
+Vous ne devez fournir que les méthodes necessaires et n'oubliez pas de commenter votre code.
---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import com.estia.mbds.Director;
+import com.estia.mbds.MBDSStaff;
+import com.estia.mbds.Secretary;
+import com.estia.mbds.Teacher;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<MBDSStaff> staffList = new ArrayList<>();
+        staffList.add(new Teacher("Gabriel Mopolo Moke"));
+        staffList.add(new Teacher("Gregory Galli"));
+        staffList.add(new Director("Michel Syska"));
+        staffList.add(new Secretary("Caroline Poirier"));
+
+        for (MBDSStaff staff : staffList)
+        {
+            staff.work();
+        }
+
+        /**
+         * Should render :
+         *
+         * I do handle the teaching part !
+         * I do handle the teaching part !
+         * I do handle the MBDS Master degree
+         * I do handle the registrations and all the paperwork
+         */
+    }
+}
